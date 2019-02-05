9d9c79963e62ecdb5582114c991e58f527f87b30
RubenVH <ruben.van.hal@kiwoluwe.be>
2019-02-05 11:55:15 +0100

    calender in User.php gezet werkt nog niet!

--- d_Site/trainer/User.php
+++ d_Site/trainer/User.php
@@ -60,12 +60,71 @@
   </section>
   <!-- END: section -->
 
-  <!-- hieronder script jquery and bootstrap http://t1m0n.name/air-datepicker/docs/ -->
+  <!-- hieronder script jquery and bootstrap https://www.solodev.com/blog/web-design/adding-a-datetime-picker-to-your-forms.stml -->
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+<script src="https://cdn.jsdelivr.net/momentjs/2.14.1/moment.min.js"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datetimepicker/4.17.37/js/bootstrap-datetimepicker.min.js"></script>
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datetimepicker/4.17.37/css/bootstrap-datetimepicker.min.css">
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
                     
                                       <section id="next-section" class="probootstrap-section">
                                         <div class="container">
-                                      
+                                        <div class="container">
+  <form>
+  <div class="panel panel-primary">
+    <div class="panel-heading">Schedule an Appointment</div>
+      <div class="panel-body">
+         <div class="row">
+            <div class="col-md-6">
+               <div class="form-group">
+                  <label class="control-label">Naam</label>
+                  <input type="text" class="form-control" name="fname" id="fname">
+               </div>
+            </div>
+            <div class="col-md-6">
+               <div class="form-group">
+                  <label class="control-label">Voornaam</label>
+                  <input type="text" class="form-control" name="lname" id="lname">
+               </div>
+            </div>
+         </div>
+         <div class="row">
+            <div class="col-md-6">
+               <div class="form-group">
+                  <label class="control-label">Email</label>
+                  <input type="text" class="form-control" name="email" id="email">
+               </div>
+            </div>
+            <div class="row">
+            <div class="col-md-6">
+               <div class="form-group">
+                  <label class="control-label">Bericht</label>
+                  <input type="Textbox" class="form-control" name="Bericht" id="Bericht">
+               </div>
+            </div>
+            <div class='col-md-6'>
+               <div class="form-group">
+                  <label class="control-label">Startdatum</label>
+                  <div class='input-group date' id='datetimepicker1'>
+                     <input type='text' class="form-control" />
+                     <span class="input-group-addon">
+                     <span class="glyphicon glyphicon-calendar"></span>
+                     </span>
+                  </div>
+               </div>
+            </div>
     
+        <input type="submit" class="btn btn-primary" value="Submit">
+      </div>
+   </div>
+</div>
+</form>
+<script>
+  $(function () {
+    $('#datetimepicker1').datetimepicker();
+ });
+</script>
                                           
 
                         </div>
